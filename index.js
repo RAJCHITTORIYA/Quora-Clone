1cbbd35d33e4102177e9110ac5d23aed9af341ff
RAJ CHITTORIYA <rajchittoriya007@gmail.com>
2025-09-20 10:01:56 +0530

    Initial commit - Quora Clone project

---
+++ index.js
@@ -0,0 +1,99 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+const path = require("path");
+
+const { v4: uuidv4 } = require('uuid');            // ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
+
+
+const methodOverride = require("method-override");
+app.use(methodOverride('_method'));
+
+
+app.use(express.urlencoded({extended : true}));
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+
+app.use(express.static(path.join(__dirname,"public")));
+
+let posts = [
+    {
+        id : uuidv4(),
+        username : "apnaCollege",
+        content : "i love coding"
+    },
+     {
+        id : uuidv4(),
+        username : "RajCollege",
+        content : "i love cricket!!"
+    },
+     {
+        id : uuidv4(),
+        username : "IIT College",
+        content : "i love placements!!"
+    },
+     {
+        id : uuidv4(),
+        username : "lovely proffessional university",
+        content : "i love money!!"
+    },
+];
+
+app.get("/posts",(req,res)=>
+{
+    res.render("index.ejs",{posts});
+});
+
+app.get("/posts/new",(req,res)=>
+{
+    res.render("new.ejs");
+});
+
+app.post("/posts",(req,res)=>
+{
+    let {username , content} = req.body;
+    let id = uuidv4();
+    posts.push({ id , username,content});
+    console.log(req.body);
+    res.redirect("/posts");
+});
+
+app.get("/posts/:id",(req,res)=>
+{
+    let {id} = req.params;
+    console.log(id);
+    let post = posts.find((p)=> id === p.id);
+    res.render("show.ejs",{post});
+});
+
+app.patch("/posts/:id", (req,res)=>
+{
+    let {id} = req.params;
+    let newContent = req.body.content;
+    let post = posts.find((p)=> id === p.id);
+    post.content = newContent;
+    console.log(post);
+
+    res.redirect("/posts");
+});
+
+
+app.get("/posts/:id/edit",(req,res)=>
+{
+    let {id} = req.params;
+    let post = posts.find((p)=> p.id === id);
+    res.render("edit.ejs",{ post });
+});
+
+app.delete("/posts/:id",(req,res)=>
+{
+    let { id} = req.params;
+    posts = posts.filter((p)=> p.id !== id);
+    res.redirect("/posts");
+})
+
+app.listen(port,()=>
+{
+    console.log("server is running on port 8080");
+});
